b4a8ef64b262bce53528d6b6c73e4b5f0e97b934
loonl <tlsddpfla@kookmin.ac.kr>
2024-01-16 15:30:56 +0900

    GameScene BattleScene으로 통합

--- Assets/C#/Area/BattlefieldSystem.cs
+++ Assets/C#/Area/BattlefieldSystem.cs
@@ -30,7 +30,7 @@ public class BattlefieldSystem : MonoBehaviour
     private Vector3 _enemygridOriginPos; // 기본: (-3, 0.1, 2.25)
 
     private const string _playerPrefabPath = "Players/Player";
-    private const string _monsterPrefabPath = "Monsters/FlyingDemon";
+    private const string _monsterPrefabPath = "Monsters/Bat";
 
     private void Awake()
     {
@@ -49,27 +49,36 @@ public class BattlefieldSystem : MonoBehaviour
 
     private void Init()
     {
+        // temp - for test
+        _playergridOriginPos = new Vector3(-3f, 0.1f, -4.75f);
+        _enemygridOriginPos = new Vector3(-3f, 0.1f, 2.25f);
+        
         _gridSystem = new BattleGridSystem(_playergridOriginPos, _enemygridOriginPos);
         BattleState = BattleState.Idle;
         Managers.InputMng.MouseAction += HandleMouseInput;
+        
         GeneratePrefabs();
     }
 
     private void GeneratePrefabs()
     {   
+        // temp - for test
+        GameObject players = new GameObject { name = "@Players" };
+        GameObject monsters = new GameObject { name = "@Monsters" };
+        
         // 현재는 테스트를 위해 그리드의 모든 셀에 프리팹을 생성하지만 추후 수정을 통해 특정 위치만 생성하도록 해야함.
-        for (int z = 0; z < _gridSystem.PlayerGrid.Height; z++)
+        for (int z = 0; z < _gridSystem.PlayerGrid.Height; z += 2) // temp - for test
         {
             for (int x = 0; x < _gridSystem.PlayerGrid.Width; x++)
             {
-                _gridSystem.PlayerGrid.InstantiatePrefab(_playerPrefabPath, x, z);
+                _gridSystem.PlayerGrid.InstantiatePrefab(_playerPrefabPath, x, z, parent: players.transform);
             }
         }
-        for (int z = 0; z < _gridSystem.EnemyGrid.Height; z++)
+        for (int z = 0; z < _gridSystem.EnemyGrid.Height; z += 2) // temp - for test
         {
             for (int x = 0; x < _gridSystem.EnemyGrid.Width; x++)
             {
-                _gridSystem.EnemyGrid.InstantiatePrefab(_monsterPrefabPath, x, z, 180);
+                _gridSystem.EnemyGrid.InstantiatePrefab(_monsterPrefabPath, x, z, 180, monsters.transform);
             }
         }
     }
