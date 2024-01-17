2f78ef5c964d2bf040c46a0289bdf43b0cf35067
Kim Taebum <ddino99@naver.com>
2024-01-17 20:45:32 +0900

    네트워크매니저, 네트워크 테스트 UI 프리팹 추가

---
+++ Assets/C#/UI/PopupUI/UI_TestClientButton.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class UI_TestClientButton : UI_Popup
+{
+    enum Buttons
+    {
+        ClientButton
+    }
+
+    private MatchmakerClient _mmc;
+    public override void Init()
+    {
+        base.Init();
+        
+        Bind<Button>(typeof(Buttons));
+        
+        GetButton(Buttons.ClientButton).gameObject.BindEvent(OnButtonClicked, Define.UIEvent.Click);
+    }
+    
+    public void OnButtonClicked(PointerEventData data)
+    {
+        Debug.Log("TestClientButton Clicked!");
+        Managers.ServerMng.StartClient();
+    }
+}
